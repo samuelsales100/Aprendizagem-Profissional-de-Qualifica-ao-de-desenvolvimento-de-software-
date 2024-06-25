751d9fe89a54bddffbb70d1448bc56a66d3dd4c2
Samuel <s4lys004@gmail.com>
2024-06-25 17:52:02 -0300

    Add files via upload

---
+++ atividade1.sql
@@ -0,0 +1,14 @@
+/* Criando tabela */
+
+create table produto(
+codigo varchar (100),
+ valor varchar (100) 
+);
+
+ /*exibindo tabela / estrutura*/
+
+describe produto;
+
+/* consulta ao banco de dados */
+
+select * from produto;
